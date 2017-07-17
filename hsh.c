29b0b4b90c7203932d1a3282c1bb65def34060aa
Andrew Birnberg <andrew.birnberg@gmail.com>
2017-07-17 09:09:12 +0000

    Updated main function and added init for struct

--- hsh.c
+++ hsh.c
@@ -1,18 +1,51 @@
 #include "hsh.h"
-#include <stdlib.h>
 
 int main(int argc, char *argv[], char *envp[])
 {
-	char *line, **command;
+	struct st_vars vars;
 
-	while (1)
+	init_vars(&vars, argv + 1, envp, NULL, NULL, NULL, 0, 0);
+
+	if (!isatty(FILENO_STDIN))
 	{
-		print_prompt();
-		line = get_next_line();
-/*		command = tokenize(line);
-		call_command(command);
-*/	}
-	free(command);
-	free(line);
-	exit(EXIT_SUCCESS);
+		get_next_line(&vars);
+		tokenize(&vars);
+		call(&vars);
+	}
+	else if (argc > 1)
+		call(&vars);
+	else
+	{
+		signal(SIGINT, SIG_IGN);
+		while (1)
+		{
+			print_prompt("Myshell$ ");
+			get_next_line(&vars);
+			tokenize(&vars);
+			call(&vars);
+		}
+	}
+	exit_and_free(&vars);
+}
+
+void init_vars(struct st_vars *vars,
+               char **command,
+	       char **envp;
+               char *lineptr,
+               char *vars,
+               alias_t *aliases,
+               ssize_t linesize,
+               ssize_t commandcount)
+{
+	vars->command = command;
+	vars->env = copyenv(envp);
+	vars->lineptr = lineptr;
+	vars->vars = vars;
+	vars->aliases = aliases;
+	vars->linesize = linesize;
+	vars->commandcount = commandcount;
+	if (hsh_setenv(vars, "PS1", "$ ", 1) == -1)
+		if (errno == ENOMEM)
+			exit_and_free(vars);
+	vars->envsize = countenv(vars->env);
 }
